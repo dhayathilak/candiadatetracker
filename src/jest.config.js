c65bd2f0a8e0e1e122d8410e08a87b7f43ec79af
dhayathilak <dhayathilak.bharathi@zemosolabs.com>
2023-01-13 16:55:49 +0530

    added coverage

--- src/jest.config.js
+++ src/jest.config.js
@@ -19,7 +19,7 @@ module.exports = {
     "<rootDir>/src/utils/commandCentermockData.js",
   ],
   transform: {
-    "^.+\\.js?$": "babel-jest",
+    '\\.[jt]sx?$': 'esbuild-jest',
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
       "./assetsTransformer.js",
   },
